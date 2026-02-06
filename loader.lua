f1a95834a69f1a9df3a94ee4b5e5296fd75b4b8b
xyzAtanas <atanasdenkov93@gmail.com>
2026-02-06 19:59:32 +0200

    loader

---
+++ loader.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 15837460390 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/xyzAtanas/starheart-loader-test/refs/heads/main/enginedemo.lua"))()
+end
+
+if game.PlaceId == 101133806907079 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/xyzAtanas/starheart-loader-test/refs/heads/main/ticktockgardens.lua"))()
+end
+
+if game.PlaceId == 126574530776259 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/xyzAtanas/starheart-loader-test/refs/heads/main/twilightterminal.lua"))()
+end
